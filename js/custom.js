cda0cf9a6e9bf6f0e9eeccb80026aef9a5326811
Kego03 <kego031130@gmail.com>
2021-12-06 16:14:41 -0500

    Init2

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,31 @@
 // Custom Js
+
+$(".text-typing").on("animationend", function(){
+    $(".text-typing").attr('style', 'display:none;');
+    $(".text-typing2").removeAttr('style');
+})
+$(".text-typing2").on("animationend", function(){
+    $(".text-typing3").removeAttr('style');
+})
+
+const toh = document.querySelector("#toHome")
+const tob = document.querySelector("#toBriefcase")
+const tow = document.querySelector("#toWhy")
+const toc = document.querySelector("#toContactUs")
+
+window.addEventListener("scroll", ()=>{
+    var scroll = scrollY;
+    console.log(scroll)
+})
+toh.addEventListener("click", ()=>{
+    window.scrollTo(0, 0);
+})
+tob.addEventListener("click", ()=>{
+    window.scrollTo(0, 906);
+})
+tow.addEventListener("click", ()=>{
+    window.scrollTo(0, 1812);
+})
+toc.addEventListener("click", ()=>{
+    window.scrollTo(0, 2718);
+})
