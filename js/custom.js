a237110d9f754faf55ef1aaa6098c22187985845
Kego03 <kego031130@gmail.com>
2021-12-06 19:43:04 -0500

    init3

--- js/custom.js
+++ js/custom.js
@@ -12,20 +12,59 @@ const toh = document.querySelector("#toHome")
 const tob = document.querySelector("#toBriefcase")
 const tow = document.querySelector("#toWhy")
 const toc = document.querySelector("#toContactUs")
+const btnD1 = document.querySelector("#btnd1")
+const btnD2 = document.querySelector("#btnd2")
+const btnD3 = document.querySelector("#btnd3")
+
+let animate1 = document.querySelectorAll(".animate1")
 
 window.addEventListener("scroll", ()=>{
-    var scroll = scrollY;
-    console.log(scroll)
-})
+    let scrollTop = document.documentElement.scrollTop;
+    for(let i= 0; i < animate1.length; i++ ){
+        let heightanimate = animate1[i].offsetTop;
+        if(heightanimate -300 < scrollTop){
+            animate1[i].classList.add("animar");
+        }
+    }
+});
+window.addEventListener("scroll", ()=>{
+    let scrollTop = document.documentElement.scrollTop;
+    for(let i= 0; i < animate1.length; i++ ){
+        let heightanimate = animate1[i].offsetTop;
+        if(heightanimate + heightanimate - 300 < scrollTop || scrollTop == 0){
+            animate1[i].classList.remove("animar");
+        }
+    }
+});
+
+
+var bsh = document.querySelector(".navbar").clientHeight;
+
 toh.addEventListener("click", ()=>{
-    window.scrollTo(0, 0);
+    let sech = document.querySelector(".secHome").offsetTop;
+    window.scrollTo(0, sech - bsh);
 })
 tob.addEventListener("click", ()=>{
-    window.scrollTo(0, 906);
+    let secb = document.querySelector(".secBriefcase").offsetTop;
+    window.scrollTo(0, secb - bsh);
+})
+btnD1.addEventListener("click", ()=>{
+    let secb = document.querySelector(".secBriefcase").offsetTop;
+    window.scrollTo(0, secb - bsh);
 })
 tow.addEventListener("click", ()=>{
-    window.scrollTo(0, 1812);
+    let secw = document.querySelector(".secWhy").offsetTop;
+    window.scrollTo(0, secw - bsh);
+})
+btnD2.addEventListener("click", ()=>{
+    let secw = document.querySelector(".secWhy").offsetTop;
+    window.scrollTo(0, secw - bsh);
 })
 toc.addEventListener("click", ()=>{
-    window.scrollTo(0, 2718);
+    let secc = document.querySelector(".secContactUs").offsetTop;
+    window.scrollTo(0, secc - bsh);
+})
+btnD3.addEventListener("click", ()=>{
+    let secc = document.querySelector(".secContactUs").offsetTop;
+    window.scrollTo(0, secc - bsh);
 })
